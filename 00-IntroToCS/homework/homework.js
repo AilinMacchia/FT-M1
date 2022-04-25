b13048f5ebf1e49202f39f95aa06eabe8d34b766
unknown <ailumacchia@hotmail.com>
2022-04-25 14:41:48 -0300

    primera homework

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -3,11 +3,30 @@
 function BinarioADecimal(num) {
   // tu codigo aca
   // algo
+  let reves= num.split('').reverse().join('') ;
+  var suma=0;
+  for(let i=0; i<reves.length; i++){
+  	suma=suma + reves[i] * 2**i;
+  }
+  return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let binario=[]
+  while (num !=0){
+  	let bin=num%2;
+  	num= Math.trunc(num/2);
+  	binario.push(bin)
+  }
+  return binario.reverse().join('')
 }
+
+// No se pueden usar: 
+// parseInt
+// toString
+
+
 
 module.exports = {
   BinarioADecimal,
